58ec8e34068b25971e5d5d2231b7c571e74cc934
chzh859@naver.com <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>
2024-03-02 15:43:11 +0900

    # 백준 코드 풀이 (1469) - backtracking 알고리즘

---
+++ src/backtracking/Q1469.java
@@ -0,0 +1,65 @@
+package backtracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/**
+ * https://www.acmicpc.net/problem/1469
+ */
+public class Q1469 {
+
+    public static int N;
+    public static int[] elements;
+    public static ArrayList<String> list = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        backtracking();
+    }
+
+    public static void backtracking() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        elements = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i=0 ; i < N; i++) elements[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(elements);
+
+        dfs_back(0, new String[N * 2], new boolean[N]);
+        System.out.println(list.size() == 0 ? "-1" : list.get(0));
+
+    }
+
+    public static void dfs_back(int depth, String[] answer, boolean[] visited) {
+        if (N * 2 == depth) {
+            list.add(String.join(" ", answer));
+            return;
+        }
+
+        if (list.size() > 0) return;
+
+        if (answer[depth] != null) {
+            dfs_back(depth + 1, answer, visited);
+            return;
+        }
+
+        for (int i=0; i < N ; i++) {
+            if (!visited[i]) {
+                int element = elements[i];
+                if (depth + element + 1 < N * 2 && answer[depth] == null && answer[depth + element + 1] == null) {
+                    visited[i] = true;
+                    answer[depth] = answer[depth + element + 1] = String.valueOf(elements[i]);
+                    dfs_back(depth + 1, answer, visited);
+                    answer[depth] = answer[depth + element + 1] = null;
+                    visited[i] = false;
+                }
+            }
+        }
+    }
+
+
+
+}
